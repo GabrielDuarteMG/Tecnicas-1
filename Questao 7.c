bbb79c6620339d34bc10ab3bc19e2e5c654a1a9d
Gabriel Duarte <gabriel160499@gmail.com>
2017-11-29 14:43:58 -0200

    Update Questao 7.c

--- Questao 7.c
+++ Questao 7.c
@@ -6,12 +6,11 @@ void main(){
     char palavra[n];
     char palavra2[n];
     scanf(" %[^\n]", palavra);
-    int len=strlen(palavra);
-    for( i = 0, k = len - 1 ; i < len; i++, k--)
+    for( i = 0, k = n - 1 ; i < n; i++, k--)
     {
     palavra2[k] = palavra[i];
     }
-    palavra2[len]='\0';
+    palavra2[n]='\0';
     if (strcmp(palavra, palavra2) == 0)
       printf("\ne palindromo");
     else
